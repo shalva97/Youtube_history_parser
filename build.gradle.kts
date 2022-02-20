61dc724a4acfba8d52ce59648a944061f2eae26c
chxei <chxei5667@gmail.com>
2022-02-20 19:06:07 +0400

    upgrade dependencies to use with jdk 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.serialization") version "1.5.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
     application
     id("groovy")
 }
