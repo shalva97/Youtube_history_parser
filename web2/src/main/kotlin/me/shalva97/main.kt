9b5568771b9133cad3f88b1bd2663c541c7541f7
George Shalvashvili <shalva258@gmail.com>
2023-01-21 18:44:21 +0400

    added simple navigation

--- web2/src/main/kotlin/me/shalva97/main.kt
+++ web2/src/main/kotlin/me/shalva97/main.kt
@@ -1,66 +1,58 @@
 package me.shalva97
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import me.shalva97.screens.DownloadsPage
+import me.shalva97.screens.HistoryPage
+import me.shalva97.screens.SettingsPage
+import me.shalva97.screens.StatsPage
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
     onWasmReady {
-        Window() {
-            Column(Modifier.fillMaxWidth().fillMaxHeight()) {
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
-                Text("blah")
+        Window {
+            var selectedDestination by remember { mutableStateOf(Destinations.HISTORY) }
+            val destinations = Destinations.values()
+            Row {
+                NavigationRail {
+
+                    FloatingActionButton(onClick = {}, shape = RoundedCornerShape(10.dp)) {
+                        Icon(Icons.Filled.Add, contentDescription = "Add")
+                    }
+
+                    Spacer(Modifier.height(16.dp))
+
+                    destinations.forEach { item ->
+                        NavigationRailItem(
+                            icon = { Icon(item.icon, contentDescription = item.title) },
+                            label = { Text(item.title) },
+                            selected = selectedDestination == item,
+                            onClick = { selectedDestination = item }
+                        )
+                    }
+                }
+                when (selectedDestination) {
+                    Destinations.HISTORY -> HistoryPage()
+                    Destinations.STATS -> StatsPage()
+                    Destinations.DOWNLOADS -> DownloadsPage()
+                    Destinations.SETTINGS -> SettingsPage()
+                }
             }
         }
     }
 }
+
+
+
