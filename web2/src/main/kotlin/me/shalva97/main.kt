a68cf3c20c1a80af7c99742d9a6e45091ad59ab5
George Shalvashvili <shalva258@gmail.com>
2023-01-28 17:10:58 +0400

    cleanup

--- web2/src/main/kotlin/me/shalva97/main.kt
+++ web2/src/main/kotlin/me/shalva97/main.kt
@@ -1,15 +1,14 @@
 package me.shalva97
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import common.selectAndParseFilesFromDisk
@@ -58,63 +57,4 @@ fun main() {
             }
         }
     }
-}
-
-fun main1() {
-
-    @Composable
-    fun TextBox(text: String = "Item") {
-        Box(
-            modifier = Modifier.height(32.dp)
-                .width(400.dp)
-                .background(color = Color(200, 0, 0, 20))
-                .padding(start = 10.dp),
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Text(text = text)
-        }
-    }
-
-    onWasmReady {
-        Window {
-            Box(
-                modifier = Modifier.fillMaxSize()
-                    .background(color = Color(180, 180, 180))
-                    .padding(10.dp)
-            ) {
-                val stateVertical = rememberScrollState(0)
-                val stateHorizontal = rememberScrollState(0)
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .verticalScroll(stateVertical)
-                        .padding(end = 12.dp, bottom = 12.dp)
-                        .horizontalScroll(stateHorizontal)
-                ) {
-                    Column {
-                        for (item in 0..30) {
-                            TextBox("Item #$item")
-                            if (item < 30) {
-                                Spacer(modifier = Modifier.height(5.dp))
-                            }
-                        }
-                    }
-                }
-                VerticalScrollbar(
-                    modifier = Modifier.align(Alignment.CenterEnd)
-                        .width(16.dp)
-                        .fillMaxHeight(),
-                    adapter = rememberScrollbarAdapter(stateVertical)
-                )
-                HorizontalScrollbar(
-                    modifier = Modifier.align(Alignment.BottomStart)
-                        .fillMaxWidth()
-                        .padding(end = 12.dp),
-                    adapter = rememberScrollbarAdapter(stateHorizontal)
-                )
-            }
-        }
-    }
-
 }
