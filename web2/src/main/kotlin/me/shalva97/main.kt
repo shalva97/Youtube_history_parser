299013c8bb79ca0cefe86104cdae9fb1a8051880
George Shalvashvili <shalva258@gmail.com>
2023-01-28 16:09:29 +0400

    added history parsing

--- web2/src/main/kotlin/me/shalva97/main.kt
+++ web2/src/main/kotlin/me/shalva97/main.kt
@@ -1,14 +1,15 @@
 package me.shalva97
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import common.selectAndParseFilesFromDisk
@@ -26,13 +27,12 @@ fun main() {
             val localScope = rememberCoroutineScope()
             var selectedDestination by remember { mutableStateOf(Destinations.HISTORY) }
             val destinations = Destinations.values()
+            var selectedFiles by remember { mutableStateOf<List<String>>(emptyList()) }
             Row {
                 NavigationRail {
-
                     FloatingActionButton(onClick = {
                         localScope.launch {
-                            val files = document.selectAndParseFilesFromDisk(".txt")
-                            println(files)
+                            selectedFiles = document.selectAndParseFilesFromDisk(".json")
                         }
                     }, shape = RoundedCornerShape(10.dp)) {
                         Icon(Icons.Filled.Add, contentDescription = "Add")
@@ -50,7 +50,7 @@ fun main() {
                     }
                 }
                 when (selectedDestination) {
-                    Destinations.HISTORY -> HistoryPage()
+                    Destinations.HISTORY -> HistoryPage(selectedFiles)
                     Destinations.STATS -> StatsPage()
                     Destinations.DOWNLOADS -> DownloadsPage()
                     Destinations.SETTINGS -> SettingsPage()
@@ -58,4 +58,63 @@ fun main() {
             }
         }
     }
+}
+
+fun main1() {
+
+    @Composable
+    fun TextBox(text: String = "Item") {
+        Box(
+            modifier = Modifier.height(32.dp)
+                .width(400.dp)
+                .background(color = Color(200, 0, 0, 20))
+                .padding(start = 10.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(text = text)
+        }
+    }
+
+    onWasmReady {
+        Window {
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .background(color = Color(180, 180, 180))
+                    .padding(10.dp)
+            ) {
+                val stateVertical = rememberScrollState(0)
+                val stateHorizontal = rememberScrollState(0)
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(stateVertical)
+                        .padding(end = 12.dp, bottom = 12.dp)
+                        .horizontalScroll(stateHorizontal)
+                ) {
+                    Column {
+                        for (item in 0..30) {
+                            TextBox("Item #$item")
+                            if (item < 30) {
+                                Spacer(modifier = Modifier.height(5.dp))
+                            }
+                        }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                        .width(16.dp)
+                        .fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(stateVertical)
+                )
+                HorizontalScrollbar(
+                    modifier = Modifier.align(Alignment.BottomStart)
+                        .fillMaxWidth()
+                        .padding(end = 12.dp),
+                    adapter = rememberScrollbarAdapter(stateHorizontal)
+                )
+            }
+        }
+    }
+
 }
--- web2/src/main/kotlin/me/shalva97/screens/hisotry.kt
+++ web2/src/main/kotlin/me/shalva97/screens/hisotry.kt
@@ -1,9 +1,41 @@
 package me.shalva97.screens
 
+import YoutubeHistory
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
-fun HistoryPage() {
-    Text("History page")
+fun HistoryPage(selectedFiles: List<String>) {
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        Modifier.padding(16.dp)
+            .verticalScroll(scrollState)
+            .draggable(orientation = Orientation.Vertical, state = rememberDraggableState { delta ->
+                coroutineScope.launch {
+                    scrollState.scrollBy(-delta)
+                }
+            })
+    ) {
+        if (selectedFiles.isNotEmpty()) {
+            val text = try {
+                YoutubeHistory(selectedFiles.first(), 10).toString()
+            } catch (e: Exception) {
+                e.message ?: "Unkown error"
+            }
+            Text(text)
+        }
+    }
 }
