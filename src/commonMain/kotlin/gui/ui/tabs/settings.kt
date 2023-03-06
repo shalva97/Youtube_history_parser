99e17549de49a4184ee2a9ceb36de464ec40f891
George Shalvashvili <shalva258@gmail.com>
2023-03-06 21:07:52 +0400

    connect SettingsScreen to settingsRepo

--- src/commonMain/kotlin/gui/ui/tabs/settings.kt
+++ src/commonMain/kotlin/gui/ui/tabs/settings.kt
@@ -8,14 +8,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import gui.data.SettingsRepo
+import org.kodein.di.compose.localDI
+import org.kodein.di.instance
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen() {
-    var maxNumberOfChar = 3
-    var value by remember {
-        mutableStateOf("10")
-    }
+
+    val settingsRepo: SettingsRepo by localDI().instance()
+
+    val minVideoClicks by settingsRepo.minimumAmountOfVideoClicks.collectAsState()
 
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
@@ -26,15 +29,16 @@ fun SettingsScreen() {
             Text(
                 text = "Minimum video clicks:"
             )
-            OutlinedTextField(
-                modifier = Modifier.height(50.dp).width(150.dp),
-                value = value,
+            OutlinedTextField(modifier = Modifier.height(50.dp).width(150.dp),
+                value = minVideoClicks.toString(),
                 singleLine = true,
                 onValueChange = { newText ->
-                    if (newText.length <= maxNumberOfChar)
-                        value = newText.filter { it.isDigit() }
-                }
-            )
+                    if (newText.length <= MAX_NUMBER_OF_CHARS) {
+                        settingsRepo.setMinClicks(newText.filter { it.isDigit() }.toInt())
+                    }
+                })
         }
     }
 }
+
+private const val MAX_NUMBER_OF_CHARS = 3
