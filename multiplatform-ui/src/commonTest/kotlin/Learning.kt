c04342db0ce10d56b8e91db3034065eeeb49378c
George Shalvashvili <shalva258@gmail.com>
2023-03-23 23:55:13 +0400

    remove web module, add new multiplatform-ui module

---
+++ multiplatform-ui/src/commonTest/kotlin/Learning.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class Learning {
+
+    @Test
+    fun someRandomLearningTest() = runTest {
+        val someRandomScope = CoroutineScope(Dispatchers.Default)
+        val selectedFiles = MutableStateFlow(123)
+        val minimumAmountOfVideoClicks = MutableStateFlow(123)
+
+        val data = selectedFiles.combine(minimumAmountOfVideoClicks) { i: Int, i1: Int -> i + i1 }
+            .onEach { println("--- before debounce") }
+            .debounce(2000)
+            .onEach { println("--- after debounce") }
+            .map(::doSomeWork)
+            .stateIn(someRandomScope, SharingStarted.Lazily, 0)
+
+        assertEquals(60516, data.take(2).last())
+        assertEquals(60516, data.first())
+    }
+
+    private fun doSomeWork(n: Int): Int {
+        return n * n
+    }
+}
