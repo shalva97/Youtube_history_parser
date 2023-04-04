05e7f6af63cf17b04f35e810703ba28bd29152cc
George Shalvashvili <shalva258@gmail.com>
2023-04-04 19:24:53 +0400

    added linuxX64

--- multiplatform-cli/src/linuxX64Main/kotlin/readFrom.kt
+++ multiplatform-cli/src/linuxX64Main/kotlin/readFrom.kt
@@ -1,3 +1,35 @@
+import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toKString
+import platform.posix.fclose
+import platform.posix.fgets
+import platform.posix.fopen
+
 actual fun readFrom(input: List<String>): List<String> {
-    TODO("Not yet implemented")
+    return input.map(::readAllText)
 }
+
+fun readAllText(filePath: String): String {
+    val returnBuffer = StringBuilder()
+
+    val file = fopen(filePath, READ_MODE) ?: throw IllegalArgumentException("Cannot open input file $filePath")
+
+    try {
+        memScoped {
+            val readBufferLength = 64 * 1024
+            val buffer = allocArray<ByteVar>(readBufferLength)
+            var line = fgets(buffer, readBufferLength, file)?.toKString()
+            while (line != null) {
+                returnBuffer.append(line)
+                line = fgets(buffer, readBufferLength, file)?.toKString()
+            }
+        }
+    } finally {
+        fclose(file)
+    }
+
+    return returnBuffer.toString()
+}
+
+const val READ_MODE = "r"
