426ab38841784d75c87b4a7a2fcdf03dd0f51aef
shalva97 <shalva258@gmail.com>
2023-04-28 20:58:28 +0400

    update compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("multiplatform") version "1.8.20" apply false
     kotlin("plugin.serialization") version "1.8.0" apply false
     id("com.adarshr.test-logger") version "3.2.0" apply false
-    id("org.jetbrains.compose") version "1.4.0-rc03" apply false
+    id("org.jetbrains.compose") version "1.4.0" apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("com.github.gmazzo.buildconfig") version "3.1.0" apply false
 }
