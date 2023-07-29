8fc6e0aa7e38689610c6675e7eeb6423431dd695
shalva97 <shalva258@gmail.com>
2023-07-29 16:15:02 +0400

    fix missing skiko lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.8.20" apply false
+    kotlin("multiplatform") version "1.9.0" apply false
     kotlin("plugin.serialization") version "1.8.0" apply false
     id("com.adarshr.test-logger") version "3.2.0" apply false
     id("org.jetbrains.compose") version "1.5.0-beta01" apply false
--- multiplatform-ui/src/commonMain/kotlin/domain/fileSelector.kt
+++ multiplatform-ui/src/commonMain/kotlin/domain/fileSelector.kt
@@ -1,7 +1,5 @@
 package domain
 
-import androidx.compose.runtime.Composable
 import models.HistoryFile
 
-@Composable
 expect fun selectAndParseFilesFromDisk(callback: (historyFile: HistoryFile) -> Unit)
